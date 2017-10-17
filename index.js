323722b2139c658cda93468c1aef510c3bf85148
Max Lynch <max@drifty.com>
2017-10-17 14:41:37 -0500

    v1

--- index.js
+++ index.js
@@ -2,69 +2,90 @@ const fs = require('fs');
 
 var ts = require('typescript');
 
-const typing = process.argv[2];
-console.log('Generating AngularJS bindings for component', typing);
-let sourceFile = ts.createSourceFile(typing, fs.readFileSync(typing).toString(), ts.ScriptTarget.ES6, /*setParentNodes */ true);
+//console.log(ts.SyntaxKind);
 
-
-const printClass = (node) => {
-  node.members.forEach(n => {
-    console.log(n.name.getText());
-    console.log(n.type.getText());
-  });
+if(process.argv.length < 5) {
+  console.error('Usage: npm run typeFile angularModuleName angularComponentPrefix');
+  process.exit(1);
 }
 
-const printTree = (root) => {
+const commandArgs = {
+  typeFile: process.argv[2],
+  angularModuleName: process.argv[3],
+  angularComponentPrefix: process.argv[4]
+};
+
+let sourceFile = ts.createSourceFile(commandArgs.typeFile, fs.readFileSync(commandArgs.typeFile).toString(), ts.ScriptTarget.ES6, /*setParentNodes */ true);
+
+const generateAngularComponent = (classNode) => {
+  const className = classNode.name.getText();
+
+  // Convert MyComponent into my-component
+  const classNameParts = className.match(/[A-Z][a-z]+/g)
+  const componentName = classNameParts.map(x => x.toLowerCase()).join('-');
+  const angularComponentName = commandArgs.angularComponentPrefix + className.substr(1);
+
+  const props = [];
+  const events = [];
+
+  classNode.members.forEach(n => {
+    const typeName = n.type.getText();
+
+    // Skip method declarations
+    if(n.kind == ts.SyntaxKind.MethodDeclaration) {
+      return;
+    }
+
+    if(typeName == 'EventEmitter') {
+      events.push(n);
+    } else {
+      props.push(n);
+    }
+  });
+
+
+  const componentText = `
+angular.module('${commandArgs.angularModuleName}')
+.component('${angularComponentName}', {
+  template: '<${componentName}></${componentName}>',
+  bindings: {
+${props.map(p => `    '${p.name.getText()}': '<',`).join('\n')}
+${events.map(p => `    '${p.name.getText()}': '&',`).join('\n')}
+  },
+  controller: function($element) {
+    var self = this;
+    const e = angular.element($element.children()[0]);
+    ${events.map(p => `
+    e.on('${p.name.getText()}', function(e) {
+      return self.${p.name.getText()}();
+    });
+
+    `)}
+    // Update props on component
+    // from Angular bindings changes
+    this.$onChanges = function(c) {
+      for(let i in c) {
+        console.log(i, c);
+        wc[i] = c[i].currentValue;
+      }
+    }
+  }
+})
+`;
+
+  console.log(componentText);
+};
+
+const walkAst = (root) => {
   p(root);
   function p(node) {
-    //console.log('Identifier', node);
     switch(node.kind) {
-      //case ts.SyntaxKind.Identifier:
       case ts.SyntaxKind.ClassDeclaration:
-        printClass(node);
+        generateAngularComponent(node);
         break;
     }
     ts.forEachChild(node, p);
   }
 }
 
-printTree(sourceFile);
-/*
-
-const funcs = Object.keys(ts).map(k => {
-  if(typeof ts[k] === 'function') { return k; }
-}).sort((a, b) => {
-  if(a < b) { return -1; }
-  if(b < a) { return 1; }
-  return 0;
-});
-
-funcs.forEach(f => console.log(f));
-*/
-
-
-/*
-const program = ts.createProgram([typing], {
-    noEmitOnError: true,
-    target: ts.ScriptTarget.ES5,
-    module: ts.ModuleKind.CommonJS
-});
-
-let emitResult = program.emit();
-
-console.log('Emit result', emitResult);
-*/
-
-/*
-let allDiagnostics = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
-
-allDiagnostics.forEach(diagnostic => {
-  console.log('Diagnostics', diagnostic);
-    let { line, character } = diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start);
-    let message = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
-    console.log(`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`);
-});
-
-let exitCode = emitResult.emitSkipped ? 1 : 0;
-
-*/
+walkAst(sourceFile);
